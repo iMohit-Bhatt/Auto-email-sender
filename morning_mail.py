500d2602d1df867d716b4df81903bd76163d5b7f
iMohit-Bhatt <mohit.bhatt.work@gmail.com>
2023-09-14 16:59:06 +0530

    Daily quotes

---
+++ morning_mail.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+import smtplib
+import random
+
+dt = datetime.now()
+day_name = dt.strftime('%A')
+
+
+if day_name == "Thursday":
+#---------------------------getting one quote:------------------------
+
+    with open("quotes.txt", "r") as datafile:
+        all_quotes = datafile.readlines()
+        quote = random.choice(all_quotes)
+
+#----------------------------sending mail------------------------------
+    # creates SMTP session
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+
+    # start TLS for security
+    s.starttls()
+
+    # Authentication
+    s.login("mohit.bhatt.work@gmail.com", "gnqulfcourgaibfe")
+
+    # message to be sent
+    message = f"Subject: {day_name} Quote! \n\n{quote}\n\n Thanks you!"
+
+    # sending the mail
+    s.sendmail("mohit.bhatt.work@gmail.com", "mohit.bhatt.work@gmail.com", message)
+
+    # terminating the session
+    s.quit()
+
+else:
+    print(f"Today is not the day to send the email to the users{day_name}")
